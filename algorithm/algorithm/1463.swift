c6e4c17c1ae1ec33e8b78388d883f1dbac797b10
a-mystic <totocaca@naver.com>
2022-05-23 23:50:35 +0900

    .

--- algorithm/algorithm/1463.swift
+++ algorithm/algorithm/1463.swift
@@ -4,17 +4,35 @@ func b_1463(N : Int) -> Int{
     var counter : Int = 0
     var input : Int = N
     while !(input == 1){
-        if (input % 3 == 0){
-            input = input / 3
-            counter = counter + 1
-        }
-        else if (input % 2 == 0){
-            input = input / 2
-            counter = counter + 1
-        }
-        else {
+        if((input>3 && (input-1) % 3 == 0) || (input>2 && (input-1) % 2 == 0)){
             input = input - 1
-            counter = counter + 1 
+            counter = counter + 1
+            if (input % 3 == 0){
+                input = input / 3
+                counter = counter + 1
+            }
+            else if (input % 2 == 0){
+                input = input / 2
+                counter = counter + 1
+            }
+            else {
+                input = input - 1
+                counter = counter + 1
+            }
+        }
+        else{
+            if (input % 3 == 0){
+                input = input / 3
+                counter = counter + 1
+            }
+            else if (input % 2 == 0){
+                input = input / 2
+                counter = counter + 1
+            }
+            else {
+                input = input - 1
+                counter = counter + 1
+            }
         }
     }
     return counter
--- algorithm/algorithm/main.swift
+++ algorithm/algorithm/main.swift
@@ -10,8 +10,3 @@ import Foundation
 private let input : Int = Int(readLine()!)!
 
 print(b_1463(N: input))
-
-let str : Int = 3
-if str is Int {
-    print("Int")
-}
