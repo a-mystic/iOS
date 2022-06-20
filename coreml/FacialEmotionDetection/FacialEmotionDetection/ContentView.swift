bede27c4f9c5fdb05a462c7f668bd82e09a59239
a-mystic <totocaca@naver.com>
2022-06-20 17:40:26 +0900

    .

---
+++ coreml/FacialEmotionDetection/FacialEmotionDetection/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FacialEmotionDetection
+//
+//  Created by mystic on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      VideoView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ coreml/FacialEmotionDetection/FacialEmotionDetection/FacialEmotionDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacialEmotionDetectionApp.swift
+//  FacialEmotionDetection
+//
+//  Created by mystic on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct FacialEmotionDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
