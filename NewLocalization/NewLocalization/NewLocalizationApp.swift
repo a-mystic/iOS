6b644c48f2973348a8382a9fa55982c73817c271
a-mystic <totocaca@naver.com>
2023-09-20 10:25:13 +0900

    new

---
+++ NewLocalization/NewLocalization/NewLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewLocalizationApp.swift
+//  NewLocalization
+//
+//  Created by a mystic on 2023/09/20.
+//
+
+import SwiftUI
+
+@main
+struct NewLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpecialColor/makeColor/makeColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  makeColorApp.swift
+//  makeColor
+//
+//  Created by a mystic on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct makeColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
