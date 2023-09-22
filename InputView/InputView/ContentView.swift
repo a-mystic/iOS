7436e0b1b671a3123d3927b18cf5aa5d833c7a83
a-mystic <totocaca@naver.com>
2023-09-22 19:32:27 +0900

    add inputview

--- InputView/InputView/ContentView.swift
+++ InputView/InputView/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let mbtis = [
+        "ISTJ", "ISFJ", "INFJ", "INTJ",
+        "ISTP", "ISFP", "INFP", "INTP",
+        "ESTP", "ESFP", "ENFP", "ENTP",
+        "ESTJ", "ESFJ", "ENFJ", "ENTJ"
+    ]
+    @AppStorage("MBTI") private var mbti: String = "None"
+    @State private var needData = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text(mbti)
+            .onAppear {
+                if mbti == "None" {
+                    needData = true
+                } else {
+                    needData = false
+                }
+            }
+            .fullScreenCover(isPresented: $needData) {
+                Form {
+                    HStack {
+                        Text("MBTI를 선택해주세요.")
+                        mbtiPicker
+                    }
+                    HStack {
+                        Spacer()
+                        submit
+                        Spacer()
+                    }
+                }
+            }
+    }
+    
+    private var mbtiPicker: some View {
+        Picker("MBTI", selection: $mbti) {
+            ForEach(mbtis, id: \.self) { mbt in
+                Text(mbt)
+            }
         }
+        .pickerStyle(.wheel)
+        .cornerRadius(15)
         .padding()
+    }
+    
+    private var submit: some View {
+        Button("submit") {
+            needData = false
+        }
+        .buttonStyle(.borderedProminent)
     }
 }
 
